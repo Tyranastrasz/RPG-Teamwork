29af13e5140500fca6c0844b87d61a2c887f68fd
r0b0pa7h <r0b0pa7@gmail.com>
2014-10-04 21:29:27 +0300

    project inport test

---
+++ UnityTest/blah/Assets/scripts/colorChanger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colorChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            this.renderer.material.color = Color.red;
+        }
+	}
+}
