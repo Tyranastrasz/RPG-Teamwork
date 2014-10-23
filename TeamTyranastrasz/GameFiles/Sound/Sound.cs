f9655c88c7bde3858d912f6829cd937dff2165db
Krassimir <xhism.kml@gmail.com>
2014-10-23 04:12:12 +0300

    Sound Magic 2

--- TeamTyranastrasz/GameFiles/Sound/Sound.cs
+++ TeamTyranastrasz/GameFiles/Sound/Sound.cs
@@ -107,14 +107,176 @@
             }
         }
 
-        public static void PlayAttackSound()
+        public static void PlayFirstSpellSound()
         {
             try
             {
-                var attackSound = new System.Windows.Media.MediaPlayer();
-                attackSound.Open(new System.Uri(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()) +
+                var firstSpellSound = new System.Windows.Media.MediaPlayer();
+                firstSpellSound.Open(new System.Uri(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()) +
                                     @"\Sound\SoundFiles\BattleAxeSwing.wav"));
-                attackSound.Play();
+                firstSpellSound.Play();
+            }
+            catch (FileNotFoundException)
+            {
+                throw new FileNotFoundException();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                throw new DirectoryNotFoundException("The path to the file is incorrect!");
+            }
+            catch (IOException)
+            {
+                throw new IOException("Cannot read from file!");
+            }
+            catch (InvalidOperationException)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public static void PlaySecondSpellSound()
+        {
+            try
+            {
+                var secondSpellSound = new System.Windows.Media.MediaPlayer();
+                secondSpellSound.Open(new System.Uri(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()) +
+                                    @"\Sound\SoundFiles\BattleAxeSwing.wav"));
+                secondSpellSound.Play();
+            }
+            catch (FileNotFoundException)
+            {
+                throw new FileNotFoundException();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                throw new DirectoryNotFoundException("The path to the file is incorrect!");
+            }
+            catch (IOException)
+            {
+                throw new IOException("Cannot read from file!");
+            }
+            catch (InvalidOperationException)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public static void PlayThirdSpellSound()
+        {
+            try
+            {
+                var thirdSpellSound = new System.Windows.Media.MediaPlayer();
+                thirdSpellSound.Open(new System.Uri(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()) +
+                                    @"\Sound\SoundFiles\BattleAxeSwing.wav"));
+                thirdSpellSound.Play();
+            }
+            catch (FileNotFoundException)
+            {
+                throw new FileNotFoundException();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                throw new DirectoryNotFoundException("The path to the file is incorrect!");
+            }
+            catch (IOException)
+            {
+                throw new IOException("Cannot read from file!");
+            }
+            catch (InvalidOperationException)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public static void PlayHealSound()
+        {
+            try
+            {
+                var healSound = new System.Windows.Media.MediaPlayer();
+                healSound.Open(new System.Uri(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()) +
+                                    @"\Sound\SoundFiles\BattleAxeSwing.wav"));
+                healSound.Play();
+            }
+            catch (FileNotFoundException)
+            {
+                throw new FileNotFoundException();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                throw new DirectoryNotFoundException("The path to the file is incorrect!");
+            }
+            catch (IOException)
+            {
+                throw new IOException("Cannot read from file!");
+            }
+            catch (InvalidOperationException)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public static void PlayAttackBuffSound()
+        {
+            try
+            {
+                var attackBuffSound = new System.Windows.Media.MediaPlayer();
+                attackBuffSound.Open(new System.Uri(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()) +
+                                    @"\Sound\SoundFiles\BattleAxeSwing.wav"));
+                attackBuffSound.Play();
+            }
+            catch (FileNotFoundException)
+            {
+                throw new FileNotFoundException();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                throw new DirectoryNotFoundException("The path to the file is incorrect!");
+            }
+            catch (IOException)
+            {
+                throw new IOException("Cannot read from file!");
+            }
+            catch (InvalidOperationException)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public static void playDefenseBuff()
+        {
+            try
+            {
+                var defenseBuffSound = new System.Windows.Media.MediaPlayer();
+                defenseBuffSound.Open(new System.Uri(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()) +
+                                    @"\Sound\SoundFiles\BattleAxeSwing.wav"));
+                defenseBuffSound.Play();
+            }
+            catch (FileNotFoundException)
+            {
+                throw new FileNotFoundException();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                throw new DirectoryNotFoundException("The path to the file is incorrect!");
+            }
+            catch (IOException)
+            {
+                throw new IOException("Cannot read from file!");
+            }
+            catch (InvalidOperationException)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public static void PlayDefendSound()
+        {
+            try
+            {
+                var defendSound = new System.Windows.Media.MediaPlayer();
+                defendSound.Open(new System.Uri(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()) +
+                                    @"\Sound\SoundFiles\BattleAxeSwing.wav"));
+                defendSound.Play();
             }
             catch (FileNotFoundException)
             {
